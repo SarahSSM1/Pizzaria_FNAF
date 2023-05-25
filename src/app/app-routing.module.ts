18a50bd7797a758ddd8612c48c262f49f6774451
SarahSSM1 <sarahsz204@gmail.com>
2023-05-25 19:28:59 -0300

    quase la

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router'
 //import { CardapioComponent } from './cardapio/cardapio.component';
 
 import { HomeComponent } from "./home/home.component";
-//import { SobreComponent } from './sobre/sobre.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 
 const APP_ROUTES:Routes =[
   {path: '', component: HomeComponent},
-  //{path: 'sobre', component: SobreComponent},
+  {path: 'sobre', component: SobreComponent},
   //{path: 'cardapio', component: CardapioComponent},
 ];
 
